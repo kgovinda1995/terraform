34cc95263889b81beeed4fd809da2eaae97076ba
kgovinda1995 <kgovinda.mech@gmail.com>
2025-10-28 08:38:29 +0530

    default rtb

--- main.tf
+++ main.tf
@@ -50,18 +50,6 @@ resource "aws_subnet" "sub-stage-2" {
     }
 }
 
-
-resource "aws_route_table" "stage-rtb" {
-      vpc_id = aws_vpc.stage-vpc.id
-
-      route {
-       cidr_block = "0.0.0.0/0"
-
-       gateway_id = aws_internet_gateway.stage-igw.id
-      }
-  
-}
-
 resource "aws_internet_gateway" "stage-igw" {
 
     vpc_id = aws_vpc.stage-vpc.id
@@ -71,14 +59,36 @@ resource "aws_internet_gateway" "stage-igw" {
         Env: var.environment
     }
 }
-resource "aws_route_table_association" "a-rtb-subnet-1" {
+
+resource "aws_default_route_table" "default-stage-rtb" {
+    default_route_table_id = aws_vpc.stage-vpc.default_route_table_id
+
+      route {
+       cidr_block = "0.0.0.0/0"
+
+       gateway_id = aws_internet_gateway.stage-igw.id
+      }
+  
+}
+/*resource "aws_route_table" "stage-rtb" {
+      vpc_id = aws_vpc.stage-vpc.id
+
+      route {
+       cidr_block = "0.0.0.0/0"
+
+       gateway_id = aws_internet_gateway.stage-igw.id
+      }
+  
+}*/
+
+/*resource "aws_route_table_association" "a-rtb-subnet-1" {
          subnet_id = aws_subnet.sub-stage-1.id
          route_table_id = aws_route_table.stage-rtb.id
 }
 resource "aws_route_table_association" "a-rtb-subnet-2" {
          subnet_id = aws_subnet.sub-stage-2.id
          route_table_id = aws_route_table.stage-rtb.id
-}
+}*/
 
 data "aws_vpc" "existing-vpc" {
    default = true
