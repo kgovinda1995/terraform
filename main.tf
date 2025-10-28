1308c9a3b30e14d960ba625f30dfebbb10625e6a
kgovinda1995 <kgovinda.mech@gmail.com>
2025-10-28 16:43:46 +0530

    entry script 1

--- main.tf
+++ main.tf
@@ -161,18 +161,16 @@ resource "aws_instance" "stage-server" {
     associate_public_ip_address = true
 
 
-    user_data = <<EOF
-                #!/bin/bash
-                sudo apt update -y && sudo apt install ca-certificates curl gnupg lsb-release -y && \
-                sudo mkdir -p /etc/apt/keyrings && \
-                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg && \
-                echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
-                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
-                sudo apt update -y && \
-                sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-                sudo usermod -aG docker ubuntu
-                docker run -p 8080:80 nginx
-                EOF
+    user_data = <<-EOF
+      #!/bin/bash
+      apt update -y
+      apt install -y docker.io
+      systemctl start docker
+      systemctl enable docker
+      usermod -aG docker ubuntu
+      echo "Docker installed successfully on $(hostname)" > /home/ubuntu/docker_setup.log
+      docker run -d -p 80:80 nginx
+    EOF
 
      tags = {
         Name : "${var.environment}-server"
