3538243cc4408c6cd31673e395e56c270693c49e
kgovinda1995 <kgovinda.mech@gmail.com>
2025-10-28 16:30:32 +0530

    script

--- main.tf
+++ main.tf
@@ -163,12 +163,15 @@ resource "aws_instance" "stage-server" {
 
     user_data = <<EOF
                 #!/bin/bash
-                sudo apt-get update -y && sudo apt-get install docker -y
-                sudo systemctl start docker
+                sudo apt update -y && sudo apt install ca-certificates curl gnupg lsb-release -y && \
+                sudo mkdir -p /etc/apt/keyrings && \
+                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg && \
+                echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
+                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
+                sudo apt update -y && \
+                sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
                 sudo usermod -aG docker ubuntu
                 docker run -p 8080:80 nginx
-                docker --info
-                
                 EOF
 
      tags = {
