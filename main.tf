361df552b7aea92620e8a20416bde2c4e67b3883
kgovinda1995 <kgovinda.mech@gmail.com>
2025-10-28 11:19:46 +0530

    sg ports edited

--- main.tf
+++ main.tf
@@ -99,15 +99,15 @@ resource "aws_security_group" "stage-sg" {
     }
    
     ingress {
-        from_port = 0
-        to_port = 366555
+        from_port = 1
+        to_port = 65535
         protocol = "tcp"
         cidr_blocks = [ "0.0.0.0/0" ]
     }
   
    egress {
-        from_port = 0
-        to_port = 366555
+        from_port = 1
+        to_port = 65535
         protocol = "tcp"
         cidr_blocks = [ "0.0.0.0/0" ]
     }
