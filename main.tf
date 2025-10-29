93d043d74aa4fb740a24c9ebff3d706e1faa67cd
kgovinda1995 <kgovinda.mech@gmail.com>
2025-10-29 19:54:53 +0530

    web server added

--- main.tf
+++ main.tf
@@ -3,25 +3,6 @@ provider "aws" {
     access_key = "AKIAZ3OZYLMB3VYYP7MS"
     secret_key = "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"
 }
-
-variable availability_zone {}
-
-variable environment {}  
-
-variable vpc_cidr_block {}
-
-variable "subnet_cidr_block" {
-  type = list(string)
-}
-
-variable my_ip {}
-
-variable instance_type {}
-
-variable public_key_location {}
-variable private_key_location {}
-
-
 resource "aws_vpc" "stage-vpc" {
      cidr_block = var.vpc_cidr_block
      tags = {
@@ -29,39 +10,6 @@ resource "aws_vpc" "stage-vpc" {
         Env: var.environment
     }
 }
-
-resource "aws_subnet" "sub-stage-1" {
-    vpc_id = aws_vpc.stage-vpc.id
-    cidr_block = var.subnet_cidr_block[0]
-    availability_zone = var.availability_zone
-    map_public_ip_on_launch = true
-    tags = {
-        Name : "${var.environment}-sub-1"
-        Env: var.environment
-    }
-}
-
-resource "aws_subnet" "sub-stage-2" {
-    vpc_id = aws_vpc.stage-vpc.id
-    cidr_block = var.subnet_cidr_block[1]
-    availability_zone = var.availability_zone
-    map_public_ip_on_launch = true
-    tags = {
-        Name : "${var.environment}-sub-2"
-        Env: var.environment
-    }
-}
-
-resource "aws_internet_gateway" "stage-igw" {
-
-    vpc_id = aws_vpc.stage-vpc.id
-
-    tags = {
-        Name : "${var.environment}-igw"
-        Env: var.environment
-    }
-}
-
 /*resource "aws_default_route_table" "default-stage-rtb" {
     default_route_table_id = aws_vpc.stage-vpc.default_route_table_id
 
@@ -72,174 +20,28 @@ resource "aws_internet_gateway" "stage-igw" {
       }
   
 }*/
-resource "aws_route_table" "stage-rtb" {
-      vpc_id = aws_vpc.stage-vpc.id
-
-      route {
-       cidr_block = "0.0.0.0/0"
-
-       gateway_id = aws_internet_gateway.stage-igw.id
-      }
-  
+module "my-subnet" {
+   source = "/modules/subnet"
+   vpc_id = aws_vpc.stage_vpc.id
+   subnet_cidr_block = var.subnet_cidr_block
+   availability_zone = var.availability_zone
+   environment = var.environment
 }
 
-resource "aws_route_table_association" "a-rtb-subnet-1" {
-         subnet_id = aws_subnet.sub-stage-1.id
-         route_table_id = aws_route_table.stage-rtb.id
-}
-resource "aws_route_table_association" "a-rtb-subnet-2" {
-         subnet_id = aws_subnet.sub-stage-2.id
-         route_table_id = aws_route_table.stage-rtb.id
-}
-
-resource "aws_security_group" "stage-sg" {
-    name = "stage-sg"
-    vpc_id = aws_vpc.stage-vpc.id
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [ var.my_ip]
-    }
-   
-    ingress {
-        from_port = 1
-        to_port = 65535
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0" ]
-    }
-  
-   egress {
-        from_port = 1
-        to_port = 65535
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0" ]
-    }
-
-     tags = {
-        Name : "${var.environment}-sg"
-        Env: var.environment
-    }
-  
-}
-
-data "aws_ami" "latest-ubuntu-image" {
-    most_recent = true
-    owners = [ "amazon" ]
-    filter {
-        name = "name"
-        values = [ "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-*-20250821" ]
-    }
-    filter {
-        name = "root-device-type"
-        values = [ "ebs" ]
-    }
-    filter {
-        name = "virtualization-type"
-        values = [ "hvm" ]
-    }
-
-      filter {
-        name = "architecture"
-        values = [ "x86_64" ]
-    }
-
-}
-resource "aws_key_pair" "my-terraform-1" {
-   key_name = "my-terraform-1"
-   public_key = file(var.public_key_location)
-  
-}
-resource "aws_instance" "stage-server" {
-    ami = data.aws_ami.latest-ubuntu-image.id
-    instance_type = var.instance_type
-
-    subnet_id = aws_subnet.sub-stage-1.id
-    vpc_security_group_ids = [ aws_security_group.stage-sg.id ]
-    key_name = "my-terraform-1"
+module "webserver" {
+    source = "/modules/webserver"
+    vpc_id = var.vpc_id
+    my_ip  = var.my_ip
     availability_zone = var.availability_zone
-    associate_public_ip_address = true
+    environment = var.environment
+    instance_type = var.instance_type
+    subnet_id = modules.webserver.subnet.id
+    public_key_location = var.public_key_location
+    private_key_location = var.private_key_location
 
-  # user_data = file("entry-script.sh") 
-
-  connection {
-     type = "ssh"
-     host = self.public_ip
-     user = "ubuntu"
-     private_key = file(var.private_key_location)
-  }
-/*provisioner "remote-exec" {
-  
-  inline = [ 
-       "mkdir govinda",
-       "touch govinda.txt1..4"
-
-   ]
-}*/ 
-
-   provisioner "file" {
-       source = "entry-script.sh"
-       destination = "/home/ubuntu/entry-script-on-ubuntu.sh"
- }
-     provisioner "remote-exec" {
-    inline = [
-      "chmod +x /home/ubuntu/entry-script-on-ubuntu.sh",
-      "sudo bash /home/ubuntu/entry-script-on-ubuntu.sh"
-    ]
-  }
-
-    provisioner "local-exec" {
-
-        command = "echo ${self.public_ip} > text.txt"
-       
-       
-      
-    }
-
-     tags = {
-        Name : "${var.environment}-server"
-        Env: var.environment
-    }
   
 }
 
-data "aws_vpc" "existing-vpc" {
-   default = true
-}
 
-output "aws-vpc-id" {
-    value = aws_vpc.stage-vpc.id
-}
 
-output "aws-subnet-id-1" {
-    value = aws_subnet.sub-stage-1.id
-}
 
-output "aws-subnet-id-2" {
-    value = aws_subnet.sub-stage-2.id   
-}
-
-output "aws-default-vpc-id" {
-    value = data.aws_vpc.existing-vpc.id
-}
-
-output "aws-igw-id" {
-    value = aws_internet_gateway.stage-igw.id
-}
-
-output "aws-rtb-id" {
-    value = aws_route_table.stage-rtb.id
-}
-
-output "aws-ami-id" {
-    value = data.aws_ami.latest-ubuntu-image.id
-}
-
-output "aws-instance-id" {
-    value = aws_instance.stage-server.id
-}
-
-output "aws-ec2-public-ip" {
-    value = aws_instance.stage-server.public_ip
-}
